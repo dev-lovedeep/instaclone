1637eb613e696dee4ecbbc43624c6afea37b99ff
lovedeep <developer.lovedeeep@gmail.com>
2020-03-27 18:14:43 +0530

    heroku minor fixes

--- follow/settings.py
+++ follow/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
 
